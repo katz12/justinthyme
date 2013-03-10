0b5c79290f147531eed2bcf16cbf94237f0f317c
Andy Katz <katz12@illinois.edu>
2013-03-10 15:26:23 -0500

    started model definition

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Hardware(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.URLField()
+    img_url = models.URLField()
+    name = models.CharField(max_length=200)
+    course = models.CharField(max_length=20, null=True, blank=True)
+    method = models.CharField(max_length=20, null=True, blank=True)
+    difficulty = models.CharField(max_length=20, null=True, blank=True)
+    wait_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    hardware = models.ManyToManyField(Hardware)
+
