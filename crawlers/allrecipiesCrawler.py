c9a930399bea5d986944c319412e4ad75ad3623f
Derek <derek@ubuntu.(none)>
2013-04-10 22:56:38 -0500

    Ingredient functionality added to Crawler, Utils.py needs to be updated

--- crawlers/allrecipiesCrawler.py
+++ crawlers/allrecipiesCrawler.py
@@ -30,9 +30,16 @@ for n in range(1, 3):
 		Cooktmin = []
 		Cookthour = []
 		Servings = []
+		MTime = 0
+		HTime = 0
+		MpTime = 0
+		HpTime = 0
+
 		#temp variables filled
 		for this in p('#itemTitle'): RecpName.append(p(this).text())
-		for this in p('#lblIngName'): IngrName.append(p(this).text())
+		for this in p('#lblIngName'): 
+			if p(this).text() != '':
+				IngrName.append(p(this).text())
 		for this in p('#lblIngAmount'): IngrAmnt.append(p(this).text())
 
 		for this in p('span.plaincharacterwrap.break'): Description.append(p(this).text())
@@ -61,20 +68,50 @@ for n in range(1, 3):
 
 
 		#parse out the decimal times
+		parsetimep = ''
+		if Cookphour:
+			parsetimep = Cookphour[0]
+		HpTime = 0
+		MpTime = 0
+		for sp in parsetimep.split():
+			if sp.isdigit():
+				HpTime = int(sp)
+			#else:
+			#	HpTime = int('0')
+		if Cookpmin:
+			parsetimep = Cookpmin[0]
+		for sp in parsetimep.split():
+			if sp.isdigit():
+				MpTime = int(sp)
+			#else:
+			#	MpTime = int('0')
+
+
+		parsetime = ''
 		if Cookthour:
 			parsetime = Cookthour[0]
-		HTime = ''
-		MTime = ''
+		HTime = 0
 		for sp in parsetime.split():
 			if sp.isdigit():
 				HTime = int(sp)
+			#else:
+			#	Htime = int('0')
+
+		parsetime = ''
 		if Cooktmin:
 			parsetime = Cooktmin[0]
+		MTime = 0
 		for sp in parsetime.split():
 			if sp.isdigit():
 				MTime = int(sp)
+			#else:
+			#	MTime = int('0')
+
 		#convert all time into minutes
+		#print HTime
+		#print MTime
 		Ctime = HTime*60 + MTime
+		Ptime = HpTime*60 + MpTime
 		if RecpName:
 			Rname = RecpName[0]
 		else:
@@ -85,6 +122,15 @@ for n in range(1, 3):
 			ImgURL = 'null'
 		
 		RecipeDict = { 'name':Rname, 'url' : nextUrl, 'img_url' : ImgURL, 
-			'cook_time' : Ctime}
-		utils.insert('recipe', **RecipeDict)
+			'cook_time' : Ctime, 'wait_time' : Ptime}
+		#utils.insert('recipe', **RecipeDict)
+		IngrDict = {}
+#		print IngrName
+#		print IngrAmnt
+		if(len(IngrName) != len(IngrAmnt)):
+			print 'Error: Number of Ingredients != Number of Ammounts'
+		else:
+			for a in range(len(IngrName)):
+				IngrDict[IngrName[a]] = IngrAmnt[a]
+		utils.insert('ingredient', **IngrDict)
 
