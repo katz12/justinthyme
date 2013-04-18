ee89e85421c4c63d5b7d218d753e862a1d7dcbf5
Derek <derek@ubuntu.(none)>
2013-04-17 23:44:17 -0500

    crawler can do more things. Refactored to look nice. Crawls w/o interupts

--- crawlers/allrecipiesCrawler.py
+++ crawlers/allrecipiesCrawler.py
@@ -1,144 +1,114 @@
-from pyquery import PyQuery as pq
+#from pyquery import PyQuery as pq
 import time
 import utils
 #change the range to (1,2205) to get all A-Z pages
 #20 results pr page
-recipe_id = 8360
-for n in range(418, 2205):
+recipe_id = 0
+for n in range(1, 220):
+    a = n * 10
+    #grab every 10th page, to provide a wide spread of results
+    urln = 'http://allrecipes.com/recipes/ViewAll.aspx?Page=' + str(a)
+    time.sleep(1)   #Sleeping is done according to the Web Crawling Standards S = pq(url=urln)
+    S = utils.getPage(urln)
+    templst = []
+    for u in S('.rectitlediv'): 
+        templst.append(S(u))
+    urlist = []
+    for u in templst: 
+        urlist.append(u.find('a').attr('href'))
+    print 'list made ' + str(n)
 
-	urln = 'http://allrecipes.com/recipes/ViewAll.aspx?Page=' + str(n)
-	time.sleep(1)			#Sleeping is done according to the Web Crawling Standards
-	S = pq(url=urln)
+    for nextUrl in urlist:
+        time.sleep(1)		#Sleeping is done according to the Web Crawling Standards
+        p = utils.getPage(nextUrl)
+        #Creation of temp variables
+        RecpName = []
+        IngrName = []
+        IngrAmnt = []
+        Description = []
+        ImageURL = []
+        Cookpmin = []
+        Cookphour = []
+        Cookcmin = []
+        Cookchour = []
+        Cooktmin = []
+        Cookthour = []
+        Servings = []
 
-	templst = []
-	for u in S('.rectitlediv'): templst.append(S(u))
-	urlist = []
-	for u in templst: urlist.append(u.find('a').attr('href'))
-	print 'list made ' + str(n)
+        #temp variables filled
+        for this in p('#itemTitle'): 
+            RecpName.append(p(this).text())
+        print RecpName
 
-	for nextUrl in urlist:
-		time.sleep(1)		#Sleeping is done according to the Web Crawling Standards
-		p = pq(url=nextUrl)
-		#Creation of temp variables
-		RecpName = []
-		IngrName = []
-		IngrAmnt = []
-		Description = []
-		ImageURL = []
-		Cookpmin = []
-		Cookphour = []
-		Cookcmin = []
-		Cookchour = []
-		Cooktmin = []
-		Cookthour = []
-		Servings = []
-		MTime = 0
-		HTime = 0
-		MpTime = 0
-		HpTime = 0
+        for this in p('#lblIngName'): 
+	        if p(this).text() != '':
+		        if p(this).attr('class') == 'ingredient-name':
+			        IngrName.append(p(this).text())
 
-		#temp variables filled
-		for this in p('#itemTitle'): RecpName.append(p(this).text())
-		for this in p('#lblIngName'): 
-			if p(this).text() != '':
-				if p(this).attr('class') == 'ingredient-name':
-					IngrName.append(p(this).text())
-		for this in p('#lblIngAmount'): IngrAmnt.append(p(this).text())
+        for this in p('#lblIngAmount'): 
+            IngrAmnt.append(p(this).text())
+        for this in p('span.plaincharacterwrap.break'): 
+            Description.append(p(this).text())
+        for this in p('#imgPhoto'): 
+            ImageURL.append(p(this).attr('src'))
 
-		for this in p('span.plaincharacterwrap.break'): Description.append(p(this).text())
-		for this in p('#imgPhoto'): ImageURL.append(p(this).attr('src'))
+        for this in p('#prepMinsSpan'): 
+            Cookpmin.append(p(this).text())
+        for this in p('#cookMinsSpan'): 
+            Cookcmin.append(p(this).text())
+        for this in p('#totalMinsSpan'): 
+            Cooktmin.append(p(this).text())
 
-		for this in p('#prepMinsSpan'): Cookpmin.append(p(this).text())
-		for this in p('#cookMinsSpan'): Cookcmin.append(p(this).text())
-		for this in p('#totalMinsSpan'): Cooktmin.append(p(this).text())
+        for this in p('#prepHoursSpan'): 
+            Cookphour.append(p(this).text())
+        for this in p('#cookHoursSpan'): 
+            Cookchour.append(p(this).text())
+        for this in p('#totalHoursSpan'): 
+            Cookthour.append(p(this).text())
 
-		for this in p('#prepHoursSpan'): Cookphour.append(p(this).text())
-		for this in p('#cookHoursSpan'): Cookchour.append(p(this).text())
-		for this in p('#totalHoursSpan'): Cookthour.append(p(this).text())
-
-		for this in p('#lblYield'): Servings.append(p(this).text())
-		#temp variables dumped into where they need to go
-		print RecpName
-#		IngrName
-#		IngrAmnt
-#		Description
-#		ImageURL
-#		Cookpmin
-#		Cookcmin
-#		Cookchour
-#		Cooktmin
-#		Cookthour
-
-
-		#parse out the decimal times
-		parsetimep = ''
-		if Cookphour:
-			parsetimep = Cookphour[0]
-		HpTime = 0
-		MpTime = 0
-		for sp in parsetimep.split():
-			if sp.isdigit():
-				HpTime = int(sp)
-
-		if Cookpmin:
-			parsetimep = Cookpmin[0]
-		for sp in parsetimep.split():
-			if sp.isdigit():
-				MpTime = int(sp)
+        for this in p('#lblYield'): 
+            Servings.append(p(this).text())
 
 
+        #parse out the decimal times for cooking
+        HpTime = utils.makeDecimalTime(Cookphour)
+        MpTime = utils.makeDecimalTime(Cookpmin)
 
-		parsetime = ''
-		if Cookthour:
-			parsetime = Cookthour[0]
-		HTime = 0
-		for sp in parsetime.split():
-			if sp.isdigit():
-				HTime = int(sp)
+        #parse out the decimal times for preping
+        HTime = utils.makeDecimalTime(Cookthour)
+        MTime = utils.makeDecimalTime(Cooktmin)
 
+        #convert all time into minutes
+        Ctime = HTime*60 + MTime
+        Ptime = HpTime*60 + MpTime
 
-		parsetime = ''
-		if Cooktmin:
-			parsetime = Cooktmin[0]
-		MTime = 0
-		for sp in parsetime.split():
-			if sp.isdigit():
-				MTime = int(sp)
+        #error checking
+        if RecpName:
+	        Rname = RecpName[0]
+        else:
+	        Rname = 'null'
+        if ImageURL:
+	        ImgURL = ImageURL[0]
+        else:
+	        ImgURL = 'null'
 
-		#convert all time into minutes
-		Ctime = HTime*60 + MTime
-		Ptime = HpTime*60 + MpTime
-		if RecpName:
-			Rname = RecpName[0]
-		else:
-			Rname = 'null'
-		if ImageURL:
-			ImgURL = ImageURL[0]
-		else:
-			ImgURL = 'null'
-		
-		RecipeDict = { 'id': recipe_id, 'name':Rname, 'url' : nextUrl, 'img_url' : ImgURL, 
-			'cook_time' : Ctime, 'wait_time' : Ptime}
-		utils.insert('recipe', **RecipeDict)
+        RecipeDict = { 'id': recipe_id, 'name':Rname, 'description' : Description, 
+            'url' : nextUrl, 'img_url' : ImgURL, 'cook_time' : Ctime, 'wait_time' : Ptime}
+        utils.insert('recipe', **RecipeDict)
 
 
-		IngrDict = {}
-		#if len(IngrName) > len(IngrAmnt):
-			#print IngrName
-			#print IngrAmnt
+        IngrDict = {}
+        #if len(IngrName) > len(IngrAmnt):
+	        #print IngrName
+	        #print IngrAmnt
 
-		for a in range(len(IngrName)):
-			if len(IngrAmnt) < len(IngrName):
-				IngrAmnt.append('') #in the case where season to preference is the 'ingredient'
-				#print len(IngrAmnt)
-			IngrDict = { 'recipe_id':recipe_id, 'ingredient_name':IngrName[a], 'quantity':IngrAmnt[a]}
-			utils.insert('recipe_ingredient', **IngrDict)
+        for a in range(len(IngrName)):
+	        if len(IngrAmnt) < len(IngrName):
+		        IngrAmnt.append('') #in the case where "season to preference" is the 'ingredient'
+		        #print len(IngrAmnt)
+	        IngrDict = { 'recipe_id':recipe_id, 'ingredient_name':IngrName[a], 'quantity':IngrAmnt[a]}
+	        utils.insert('recipe_ingredient', **IngrDict)
 
-		#--------build the ingredient list---------
-		
-		
+        #--------build the ingredient list---------
 
-
-
-
-		recipe_id += 1
+        recipe_id += 1
