3502af0b7034b442a28884d728ba69c89edd33df
chrzano1 <chrzano1@illinois.edu>
2013-04-21 17:41:07 -0500

    urls doh

--- justinthyme/urls.py
+++ justinthyme/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
 
     # Kate add URLs here
     url(r'^site/$', 'recipe_app.views.index', name='index'),
-    url(r'site/contacts/$', 'recipe_app.views.contact'),
+    url(r'site/contacts/$', 'recipe_app.views.contact', name='contact'),
 
     # Michal add URLs here
 
